037f1578b38c2901272a5c1b3cd3a65fc925f3d8
ronaldoFlores3552 <ronaldo.flores@utec.edu.pe>
2023-09-07 11:43:57 -0500

    version final Js con Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,92 @@
 <script setup>
-import {ref, computed} from "vue";
-const name="Vue 3"; 
-const contador= ref(0);
+import { ref, computed } from "vue";
+const name = "Vue 3";
+const contador = ref(0);
 
-const arrayNumeros=ref([]);
+const arrayNumeros = ref([]);
 
-const handClick=(mensaje)=>{
+const handClick = (mensaje) => {
   console.log(mensaje);
 }
 
-const Aumentar=()=>{
+const Aumentar = () => {
   contador.value++;
 }
-const Disminuir=()=>{
+const Disminuir = () => {
   contador.value--;
 }
 
-const computabilidad = computed(()=> {
-  if(contador.value===0){
+const computabilidad = computed(() => {
+  if (contador.value === 0) {
     return 'zero'
   }
-  else if(contador.value>0){
+  else if (contador.value > 0) {
     return 'positive'
-  }else{
+  } else {
     return 'negative';
   }
 });
 
-const add=()=>{
+const add = () => {
   arrayNumeros.value.push(contador.value);
 }
 
-const blockBtn= computed(()=>{
-  const numero= arrayNumeros.value.find((num)=>num === contador.value);
-  return numero || numero===0;
+const blockBtn = computed(() => {
+  const numero = arrayNumeros.value.find((num) => num === contador.value);
+  return numero || numero === 0;
 });
 
 </script>
 
 <template>
-  <h1>
-    Mi {{name.toUpperCase()}}
-  </h1>
-  <!--
+  <div class="container text-center mt-5">
+
+    <h1>
+      Mi {{ name.toUpperCase() }}
+    </h1>
+    <!--
     <button v-on:click="handClick">
       Activado
     </button> 
   -->
-  <h2 v-bind:class="computabilidad">{{ contador }}</h2>
+    <h2 v-bind:class="computabilidad">{{ contador }}</h2>
 
-  <button @click="Aumentar">
-    Aumentar
-  </button>
+    <div class="btn-group">
+      <button @click="Aumentar" class="btn btn-success">
+        Aumentar
+      </button>
 
-  <button @click="Disminuir">
-    Disminuir
-  </button>
+      <button @click="Disminuir" class="btn btn-danger">
+        Disminuir
+      </button>
 
-  <button @click="add" v-bind:disabled="blockBtn">
-    Agregar
-  </button>
+      <button @click="add" v-bind:disabled="blockBtn" class="btn btn-primary">
+        Agregar
+      </button>
+    </div>
+    <div>
+      <h2 class="mt-3">Mis Favoritos</h2>
+      <ul class="list-group mt-2">
+        <li class="list-group-item" v-for="numero in arrayNumeros" key="numero">
+          {{ numero }}
+        </li>
+      </ul>
 
-  <ul>
-    <li v-for="numero in arrayNumeros" key="numero">
-        {{ numero }}
-    </li>
-  </ul>
+    </div>
 
+  </div>
 </template>
 
 <style>
-.positive{
-  color:green;
+.positive {
+  color: green;
 }
-.negative{
-  color:red;
+
+.negative {
+  color: red;
 }
-.zero{
+
+.zero {
   color: peru;
 }
 </style>
